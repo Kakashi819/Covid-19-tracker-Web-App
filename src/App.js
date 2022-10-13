e0940845d9f95a724fa7c1d801eadf3b60691241
Ayush Singh <ayushsinghkushwah8@gmail.com>
2022-10-14 01:23:41 +0530

    created header section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { MenuItem, FormControl, Select } from "@mui/material";
 
 function App() {
+
+  const [country, setCountry] = useState('WorldWide');
+  const [countries, setCountries] = useState([]);
+
+  //i want to display all countries and change when something is changed
+  useEffect(() => {
+
+    const getCountryData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+        setCountries(countries); // update countries variable
+        });
+    };
+    getCountryData(); // call async function
+  }, [countries]);
+
+
+  const onCountryChange =(event)=>{
+    const countryCode =event.target.value;
+    setCountry(countryCode);
+  }
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app_header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app_dropdown">
+
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+            <MenuItem value="Worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+
+        </FormControl>
+      </div>
     </div>
   );
 }
